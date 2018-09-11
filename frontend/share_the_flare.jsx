f41edfae89c5c4d71c7cc24ebdbbf887470afaaf
Jean Woo Ha <jeanwoo.ha@rutgers.edu>
2018-09-11 14:37:26 -0400

    "Fix gemfile, rails server works now"

--- frontend/share_the_flare.jsx
+++ frontend/share_the_flare.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+window.login = login
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Share The Flare</h1>, root);
