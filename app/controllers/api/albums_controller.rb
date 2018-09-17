46f4bc00d41412312d5a5000bbd99a896dbb1eb1
Jean Woo Ha <jeanwoo.ha@rutgers.edu>
2018-09-16 21:27:10 -0400

    "Create photoalbum joins table, write albums_controller"

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,2 +1,42 @@
 class Api::AlbumsController < ApplicationController
+  def index
+    @albums = Album.all;
+  end
+
+  def new
+    @album = Album.new;
+  end
+
+  def create
+    @album = Album.new(album_params);
+    @album.owner_id = current_user.id;
+    if @album.save
+      render :show
+    else
+      render :json @album.errors.full_messages, status: 422
+    end
+  end
+  
+  def show
+    @album = Album.find(params[:id]);
+  end
+
+  def update
+    @album = current_user.albums.find(params[:id]);
+    @album.update(album_params);
+    if @album
+      render :show
+    else
+      render :json
+    end
+  end
+
+  def destroy
+    
+  end
+
+  def album_params
+    params.require(:album).permit(:owner_id, :title);
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_193909) do
+ActiveRecord::Schema.define(version: 2018_09_17_012224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2018_09_14_193909) do
     t.datetime "updated_at", null: false
     t.index ["commenter_id"], name: "index_comments_on_commenter_id"
     t.index ["photo_id"], name: "index_comments_on_photo_id"
+  end
+
+  create_table "photo_albums", force: :cascade do |t|
+    t.integer "photo_id", null: false
+    t.integer "album_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id", "album_id"], name: "index_photo_albums_on_photo_id_and_album_id", unique: true
   end
 
   create_table "photos", force: :cascade do |t|
