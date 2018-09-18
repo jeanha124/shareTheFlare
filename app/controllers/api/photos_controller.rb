3dc7046612827eca4b772002452a4d5d397d15ff
Jean Woo Ha <jeanwoo.ha@rutgers.edu>
2018-09-18 11:55:06 -0400

    "Fix Photos Controller"

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -13,7 +13,7 @@ class Api::PhotosController < ApplicationController
     if @photo.save
       render :show
     else
-      render :json @photo.errors.full_messages, status: 422
+      render json: @photo.errors.full_messages, status: 422
     end
 
   end
