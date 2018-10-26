b289401ecd595dba3f3b023a40c72505dcdf2683
Jean Woo Ha <jeanwoo.ha@rutgers.edu>
2018-10-26 10:45:11 -0400

    "Add activity to Hompage and delete 2 previous files"

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -405,120 +405,14 @@ function (_React$Component) {
 
 /***/ }),
 
-/***/ "./frontend/components/homepage/activity_photos.jsx":
-/*!**********************************************************!*\
-  !*** ./frontend/components/homepage/activity_photos.jsx ***!
-  \**********************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _photos_photo_index_item__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../photos/photo_index_item */ "./frontend/components/photos/photo_index_item.jsx");
-/* harmony import */ var _main_tools_main_nav_container__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../main_tools/main_nav_container */ "./frontend/components/main_tools/main_nav_container.jsx");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-
-
-
-
-var ActivityPhotos =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(ActivityPhotos, _React$Component);
-
-  function ActivityPhotos(props) {
-    var _this;
-
-    _classCallCheck(this, ActivityPhotos);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(ActivityPhotos).call(this, props));
-    _this.currentUser = _this.props.currentUser;
-    return _this;
-  }
-
-  _createClass(ActivityPhotos, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      this.props.receiveAllPhotos();
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this2 = this;
-
-      var photos = this.props.photos.map(function (photo) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_photos_photo_index_item__WEBPACK_IMPORTED_MODULE_1__["default"], {
-          key: photo.id,
-          currentUser: _this2.currentUser,
-          photo: photo,
-          receivePhoto: _this2.props.receivePhoto
-        });
-      });
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_main_tools_main_nav_container__WEBPACK_IMPORTED_MODULE_2__["default"], null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "photo-div"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, photos)));
-    }
-  }]);
-
-  return ActivityPhotos;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
-
-/* harmony default export */ __webpack_exports__["default"] = (ActivityPhotos);
-
-/***/ }),
-
 /***/ "./frontend/components/homepage/activity_photos_container.js":
 /*!*******************************************************************!*\
   !*** ./frontend/components/homepage/activity_photos_container.js ***!
   \*******************************************************************/
 /*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _activity_photos__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./activity_photos */ "./frontend/components/homepage/activity_photos.jsx");
-/* harmony import */ var _actions_photo_actions__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../actions/photo_actions */ "./frontend/actions/photo_actions.js");
-
-
-
-
-var msp = function msp(state) {
-  return {
-    currentUser: state.entities.users[state.session.id],
-    photos: Object.values(state.entities.photos),
-    users: state.entities.users
-  };
-};
-
-var mdp = function mdp(dispatch) {
-  return {
-    receiveAllPhotos: function receiveAllPhotos() {
-      return dispatch(Object(_actions_photo_actions__WEBPACK_IMPORTED_MODULE_2__["receiveAllPhotos"])());
-    }
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(msp, mdp)(_activity_photos__WEBPACK_IMPORTED_MODULE_1__["default"]));
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/Jeanwoo/Desktop/App_Academy/shareTheFlare/frontend/components/homepage/activity_photos_container.js'");
 
 /***/ }),
 
@@ -568,6 +462,7 @@ var mdp = function mdp(dispatch) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _photos_photo_index_item__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../photos/photo_index_item */ "./frontend/components/photos/photo_index_item.jsx");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -588,21 +483,47 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 
 
+
 var Homepage =
 /*#__PURE__*/
 function (_React$Component) {
   _inherits(Homepage, _React$Component);
 
-  function Homepage() {
+  function Homepage(props) {
+    var _this;
+
     _classCallCheck(this, Homepage);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Homepage).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Homepage).call(this, props));
+    _this.currentUser = _this.props.currentUser;
+    return _this;
   }
 
   _createClass(Homepage, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.props.receiveAllPhotos();
+    }
+  }, {
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null);
+      var _this2 = this;
+
+      var photos = this.props.photos.map(function (photo) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_photos_photo_index_item__WEBPACK_IMPORTED_MODULE_1__["default"], {
+          key: photo.id,
+          currentUser: _this2.currentUser,
+          photo: photo,
+          receivePhoto: _this2.props.receivePhoto
+        });
+      });
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", {
+        className: "sub-nav"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+        className: "activity"
+      }, "All Activity")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "photo-div"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, photos)));
     }
   }]);
 
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PhotoIndexItem from '../photos/photo_index_item';
+
 
 class Homepage extends React.Component {
+  constructor(props){
+    super(props);
+    this.currentUser = this.props.currentUser;
+  }
+  componentDidMount(){
+    this.props.receiveAllPhotos();
+  }
   render (){
+    const photos = this.props.photos.map(photo => {
+      return <PhotoIndexItem key={photo.id} currentUser={this.currentUser} photo={photo} receivePhoto={this.props.receivePhoto} />;
+    });
    return (
     <React.Fragment>
-      
+      <nav className="sub-nav">
+          <h1 className="activity">All Activity</h1>
+      </nav>
+      <div className="photo-div">
+        <ul>{photos}</ul>
+      </div>
     </React.Fragment>
    );
   }
